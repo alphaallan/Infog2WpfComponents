1939dc9c1c38cc97d9d67683002ca54ba27f9b0a
Allan Leon A. Leitão <alfaallan@gmail.com>
2014-11-11 16:50:45 -0300

    Adicionado Icone opcional no MyButton

--- InfoG2WpfControls/MyButton.cs
+++ InfoG2WpfControls/MyButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,5 +30,18 @@ namespace InfoG2WpfControls
         }
         public static readonly DependencyProperty CornerRadiusProperty =
             DependencyProperty.Register("CornerRadius", typeof(double), typeof(MyButton), new PropertyMetadata(2.0));
+
+        //Propriedades do icone
+        #region Icon
+        //Caminho do icone
+        [Category("Icon")]
+        public ImageSource Icon
+        {
+            get { return (ImageSource)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(ImageSource), typeof(MyButton), new PropertyMetadata(null));
+        #endregion Icon
     }
 }
