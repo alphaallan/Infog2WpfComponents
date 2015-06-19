9d2e5a52f5d0f96c7baf58a878d28b1141b673a9
Allan Leon A. Leitão <alfaallan@gmail.com>
2015-06-19 10:48:14 -0300

    Incluido Novo controle ChartPlotter, Editado Cores de fundo do data grid no tema infodark Removido controle não utilizado MyTextBoxLabel Adicionado comportamento reversível no conversor BoolToVisibility

--- InfoG2WpfControls/BoolToVisibility.cs
+++ InfoG2WpfControls/BoolToVisibility.cs
@@ -10,8 +10,26 @@ namespace InfoG2WpfControls
 {
     public class BoolToVisibility : IValueConverter
     {
+        /// <summary>
+        /// Converter um booleano em Visibility
+        /// 
+        /// Obs. Se o value não for do tipo bool retorna Visibility.Visible
+        /// </summary>
+        /// <param name="value">True = Visibility.Visible, False = Visibility.Collapsed</param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter">True = Inverter funcionamento, False ou NULL funcionamento normal</param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
+            bool temp;
+
+            if (Boolean.TryParse(parameter as string, out temp) && temp)
+            {
+                if (value is bool) return (((bool)value) ? Visibility.Collapsed : Visibility.Visible);
+                else return Visibility.Collapsed;
+            }
+            
             if (value is bool) return (((bool)value) ? Visibility.Visible : Visibility.Collapsed );
             else return Visibility.Visible;
         }
