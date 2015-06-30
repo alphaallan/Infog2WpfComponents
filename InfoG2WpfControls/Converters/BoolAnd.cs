7f2cad11f8c5f3fd65dbee23a850f7d80b64886f
Allan Leon A. Leitão <alfaallan@gmail.com>
2015-06-30 10:08:14 -0300

    Adicionado conversores BoolAnd, EnumEqual e EnumNotEquals

---
+++ InfoG2WpfControls/Converters/BoolAnd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace InfoG2WpfControls
+{
+    public class BoolAnd : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (values.All(v => (v is bool && (bool)v)));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InfoG2WpfControls/Converters/EnumNotEquals.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace InfoG2WpfControls
+{
+    public class EnumNotEquals : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is Enum)
+            {
+                if (parameter is string)
+                {
+                    try
+                    {
+                        return !(value.Equals(Enum.Parse(value.GetType(), (string)parameter)));
+                    }
+
+                    catch (ArgumentException exception)
+                    {
+                        System.Diagnostics.Debug.WriteLine(
+                            string.Format("EnumBinding = {0},  EnumValue = {1},  ArgumentException {2}",
+                                            value, parameter, exception));
+                        throw;
+                    }
+                }
+                else if (parameter is Array && (parameter as Array).Length > 0 && (parameter as Array).GetValue(0) is string)
+                {
+                    foreach (string s in (parameter as Array))
+                    {
+                        try
+                        {
+                            if (value.Equals(Enum.Parse(value.GetType(), s))) return false;
+                        }
+
+                        catch (ArgumentException exception)
+                        {
+                            System.Diagnostics.Debug.WriteLine(
+                                string.Format("EnumBinding = {0},  EnumValue = {1},  ArgumentException {2}",
+                                                value, parameter, exception));
+                            throw;
+                        }
+                    }
+
+                    return true;
+                }
+
+                return false;
+            }
+            else return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
