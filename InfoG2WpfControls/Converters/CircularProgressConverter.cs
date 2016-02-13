6d3412cdb3596fd09ca680922f05098b28e4b7e2
Allan Leon Alencar Leitão <alfaallan@gmail.com>
2016-02-13 08:58:52 -0300

    Adicionado arquivo de templates de controles

---
+++ InfoG2WpfControls/Converters/CircularProgressConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace InfoG2WpfControls
+{
+    class CircularProgressConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double progress = (double)values[0];
+            ProgressBar progressBar = values[1] as ProgressBar;
+            string type = (string)parameter;
+
+            return 359.999 * (progress / (progressBar.Maximum - progressBar.Minimum));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
